77b84a5a54388c639ea27633f88262ab14b9069f
Aran Ink <accounts@aran.ink>
2026-06-16 23:33:59 -0400

    use screen UVs for dissolve shaders

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781666490|1815834633';
+const CACHE_VERSION = '1781667203|2528809160';
 /** @type {string} */
 const CACHE_PREFIX = 'The Court of Sta-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
