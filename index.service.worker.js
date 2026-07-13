e0ebbcbe58de75beb370d3f59028666f6a82a689
Aran P. Ink <accounts@aran.ink>
2026-07-13 00:34:57 -0400

    add... drawing!? To yonder's character page, at least.

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783146089|12202439172';
+const CACHE_VERSION = '1783916026|13354776819';
 /** @type {string} */
 const CACHE_PREFIX = 'The Court of Sta-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
